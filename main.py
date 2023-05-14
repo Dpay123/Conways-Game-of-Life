242bc43dcf7722d37bf537949e0a3c4bbc125d49
Dpay <payerdom@my.uri.edu>
2023-05-14 15:28:02 -0400

    Add glider

--- main.py
+++ main.py
@@ -5,20 +5,28 @@ class Board:
         self.width = w
         self.height = h
         self.cells = []
-        self.set_random()
 
     def set_random(self):
         for x in range(self.height):
             column = []
             for y in range(self.width):
                 if random.randint(0, 1) == 0:
-                    column.append('#')
+                    column.append('[O]')
                 else:
-                    column.append(' ')
+                    column.append('[ ]')
+            self.cells.append(column)
+    def set_glider(self):
+        for x in range(self.height):
+            column = []
+            for y in range(self.width):
+                if (x, y) in ((1, 0), (2, 1), (0, 2), (1, 2), (2, 2)):
+                    column.append('[O]')
+                else:
+                    column.append('[ ]')
             self.cells.append(column)
 
     def show_board(self):
-        print('\n\n\n\n\n')
+        print('\n\n')
         for x in range(self.height):
             for y in range(self.width):
                 print(self.cells[x][y], end='')
@@ -31,43 +39,52 @@ class Board:
 
             for x in range(self.height):
                 for y in range(self.width):
-                    left = max(0, y - 1)
-                    right = min(y + 1, self.width - 1)
-                    up = max(0, x - 1)
-                    down = min(x + 1, self.height - 1)
+                    left = (y - 1) % self.width
+                    right = (y + 1) % self.width
+                    up = (x - 1) % self.height
+                    down = (x + 1) % self.height
 
                     numNeighbors = 0
-                    if currentCells[up][left] == '#':
+                    if currentCells[up][left] == '[O]':
                         numNeighbors += 1
-                    if currentCells[up][y] == '#':
+                    if currentCells[up][y] == '[O]':
                         numNeighbors += 1
-                    if currentCells[up][right] == '#':
+                    if currentCells[up][right] == '[O]':
                         numNeighbors += 1
-                    if currentCells[x][left] == '#':
+                    if currentCells[x][left] == '[O]':
                         numNeighbors += 1
-                    if currentCells[x][right] == '#':
+                    if currentCells[x][right] == '[O]':
                         numNeighbors += 1
-                    if currentCells[down][left] == '#':
+                    if currentCells[down][left] == '[O]':
                         numNeighbors += 1
-                    if currentCells[down][y] == '#':
+                    if currentCells[down][y] == '[O]':
                         numNeighbors += 1
-                    if currentCells[down][right] == '#':
+                    if currentCells[down][right] == '[O]':
                         numNeighbors += 1
 
                     cell = currentCells[x][y]
 
-                    if cell == '#' and numNeighbors in [2, 3]:
+                    if cell == '[O]' and numNeighbors in [2, 3]:
                         continue
-                    elif cell == ' ' and numNeighbors == 3:
-                        self.cells[x][y] = '#'
+                    elif cell == '[ ]' and numNeighbors == 3:
+                        self.cells[x][y] = '[O]'
                     else:
-                        self.cells[x][y] = ' '
+                        self.cells[x][y] = '[ ]'
 
-            time.sleep(0.5)
+            time.sleep(.5)
 
 
 
 if __name__ == '__main__':
     print('Conways Game of Life')
     board = Board()
+    while True:
+        print('Choose an option:' + '\n(r)andom' + '\n(g)lider')
+        choice = input()
+        if choice == 'r':
+            board.set_random()
+            break
+        elif choice == 'g':
+            board.set_glider()
+            break
     board.run()
